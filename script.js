087e45204d337cb7f45d4cec4d0c76253bcc87a0
Amit Hariyale <57282097+Amithariyale@users.noreply.github.com>
2023-12-29 17:34:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,3 +6,33 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
+toggle.addEventListener("click", (e) => {
+  if (video.paused) {
+    video.play();
+    toggle.innerText = "pause";
+  } else {
+    video.pause();
+    toggle.innerText = "play_arrow";
+  }
+});
+
+video.addEventListener("timeupdate", () => {
+  let width = (video.currentTime * 100) / video.duration;
+  progressBar.style.width = `${width}%`;
+});
+
+ranges[0].addEventListener("input", (e) => {
+  video.volume = e.target.value;
+});
+
+ranges[1].addEventListener("change", (e) => {
+  video.playbackRate = e.target.value;
+});
+
+skipButtons[0].addEventListener("click", () => {
+  video.currentTime -= 10;
+});
+skipButtons[1].addEventListener("click", () => {
+  video.currentTime += 25;
+});
